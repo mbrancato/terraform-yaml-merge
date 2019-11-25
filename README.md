f3664966bd29cb2ea801477b716696c1ec6da7fe
Mike Brancato <mike@mikebrancato.com>
2019-11-24 22:12:24 -0800

    Add initial module code

---
+++ README.md
@@ -0,0 +1,26 @@
+# terraform-yaml-merge-lists
+
+This module can be used to merge two YAML files. The goal was to allow merging
+of certain configurations without the need of an external program such as `yq`
+to do the merging.
+
+## Compatibility
+
+There are multiple strategies for merging YAML data. The approach here is to
+combine lists of strings in the top-level keys of the data, if the keys exist
+in both sets of YAML data being merged. Supported types for merging include
+only those that may be converted to a string including internal Terraform types
+of string, number and bool. For keys unique to both sets of data, their data is
+included in the result as the original type. If there is a conflict for a
+supported non-list type, it will be returned as a list of strings in the
+result. Anything consuming the result must understand that certain types, such
+as numbers, are going to be returned as a list of strings.
+
+For example, if the first set has `name: foo` and the second set has
+`name: bar`, the result would be:
+
+```
+name:
+- foo
+- bar
+```
---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">=0.12"
+}
+
+variable "input" {
+  type = list(string)
+}
+
+variable "dedupe" {
+  type    = bool
+  default = false
+}
+
+locals {
+  alpha      = yamldecode(var.input[0])
+  beta       = yamldecode(var.input[1])
+  alphakeys  = [ for key, val in local.alpha : key ]
+  betakeys   = [ for key, val in local.beta : key ]
+  commonkeys = setintersection(local.alphakeys, local.betakeys)
+  merged = merge(local.alpha, local.beta,
+    {
+      for key in local.commonkeys :
+      key => var.dedupe ?
+      distinct(
+        compact(concat(
+          # If not a list, convert to list with single element
+          split(
+            ":::::::", [flatten([for x in [local.alpha[key]] : x])[0]] ==
+            [[for x in [local.alpha[key]] : x][0]] ?
+            tostring(local.alpha[key]) : join(":::::::", local.alpha[key])
+          ),
+          split(
+            ":::::::", [flatten([for x in [local.beta[key]] : x])[0]] ==
+            [[for x in [local.beta[key]] : x][0]] ?
+            tostring(local.beta[key]) : join(":::::::", local.beta[key])
+          )
+        ))
+      ) :
+      compact(concat(
+        # If not a list, convert to list with single element
+        split(
+          ":::::::", [flatten([for x in [local.alpha[key]] : x])[0]] ==
+          [[for x in [local.alpha[key]] : x][0]] ?
+          tostring(local.alpha[key]) : join(":::::::", local.alpha[key])
+        ),
+        split(
+          ":::::::", [flatten([for x in [local.beta[key]] : x])[0]] ==
+          [[for x in [local.beta[key]] : x][0]] ?
+          tostring(local.beta[key]) : join(":::::::", local.beta[key])
+        )
+      ))
+    }
+  )
+}
+
+output "result" {
+  value = yamlencode(local.merged)
+}
